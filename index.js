5a97975730e9e8dd38aa8bee3ae29cdb0d684e66
MisaelSantana <misaelsantana2011hotmail.com>
2021-09-22 17:59:35 -0300

    add body parser in project challenge

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+// Set dependencies in project;
 const { response, request } = require('express');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+// Config EJS view Express how template engine;
+app.set('view engine', 'ejs');
+
+// Decode the submission of data via the forms;
 app.use(express.urlencoded({extended: false }));
 app.use(express.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
 
 // Create page home;
 app.get('/home', (request, response) => {
-    response.send('Funcionando');
+    response.send('Funcionando...');
 });
 
 // Redirect for page home;
